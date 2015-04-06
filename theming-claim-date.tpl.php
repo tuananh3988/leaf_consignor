164aad4170d82b16a579ab6d6f9e2454ea09f228
Home <tuananh3988@gmail.com>
2015-04-07 01:58:54 +0700

    claim date finish

---
+++ theming-claim-date.tpl.php
@@ -0,0 +1,11 @@
+<div style="background:#f6f6f6;font-family:Verdana,Arial,Helvetica,sans-serif;font-size:12px;margin:0;padding:0">
+Hello <?= $vendor['name']; ?>!<br><br>
+<p>Your contract #<?= $contract['contract_uuid']; ?> is now expired. The attached file lists all the details of your contract!</p>
+<p>You can expect your commission check for any sold items to arrive 30 days from your claim date, which is the end of your contract term.</p>
+
+<p>Again, thanks for consigning with Greene Street!</p>
+<p>The Greene Street Online Team</p>
+<p><a href="http://www.shopgreenestreet.com" target="_blank">www.shopgreenestreet.com</a></p>
+<p>Any questions? You can reach us at:</p>
+<p><a href="mailto:onlinecustomerservice@greenestreet.com" style="color:blue" target="_blank">onlinecustomerservice@<wbr>greenestreet.com </a></p>
+</div>

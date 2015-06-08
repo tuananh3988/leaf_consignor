4f8fb4f2bf2803533961e15c5b89a0712f4a71a3
anhct-seta <anhct6285@seta-asia.com.vn>
2015-06-08 17:24:21 +0700

    update vendor detail popup

---
+++ vendor-detail-popup.tpl.php
@@ -0,0 +1,73 @@
+<h2>ID : #<?= $vendor['vendor_uuid']; ?></h2>
+<table class="ajaxview_vendor">
+
+    <tbody><tr>
+        <td>
+            <label>Name: </label>
+            <span><?= $vendor['name']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Address: </label>
+            <span><?= $vendor['address']; ?></span>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label>City: </label>
+            <span><?= $vendor['city']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>State: </label>
+            <span><?= $vendor['state']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Zip: </label>
+            <span><?= $vendor['zip']; ?></span>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label>Email Address: </label>
+            <span><?= $vendor['email_address']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Phone Number: </label>
+            <span><?= $vendor['phone_number']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Alternate Phone Number: </label>
+            <span><?= $vendor['alternate_phone_number']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Payment Method: </label>
+            <span><?= $vendor['vendor_uuid']; ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Active contracts: </label>
+            <span>0</span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>Contracts: </label>
+            <span>2</span>
+        </td>
+    </tr>
+</tbody>
+</table>
